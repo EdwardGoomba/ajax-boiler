8891ec8b470fa67e406fb9f359d49ed04a416b7f
EdwardGoomba <ask@edwardvdesign.com>
2018-01-09 18:57:25 -0500

    Added async post request

--- main.js
+++ main.js
@@ -84,3 +84,22 @@ async function getData() {
     console.log(error);
   }
 }
+
+// async POST request
+
+async function getData() {
+  try {
+    let response = await fetch('https://api-to-call.com/endpoint', {
+      method: 'POST',
+      body: JSON.stringify({id: 200})
+    })
+		if (response.ok) {
+  		let jsonResponse = await response.json();
+  		return jsonResponse;
+		}
+    throw new Error('Request failed!');
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
