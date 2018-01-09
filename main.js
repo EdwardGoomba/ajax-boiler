aca96162a280fda82721471b60c0c407b86e71dd
EdwardGoomba <ask@edwardvdesign.com>
2018-01-09 16:22:08 -0500

    Added fetch post request

--- main.js
+++ main.js
@@ -40,3 +40,16 @@ $.ajax({
     console.log(jqXHR);
   }
 });
+
+// fetch post requst
+
+fetch('https://api-to-call.com/endpoint').then(response => {
+		if (response.ok) {
+			return response.json();
+		}
+		throw new Error('Request failed!');
+	},
+	networkError => {
+		console.log(networkError.message);
+	}
+).then(jsonResponse => jsonResponse);
