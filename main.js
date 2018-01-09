0ddad0895fbc9d6ea6b37f5b4eea15cd6e74d408
EdwardGoomba <ask@edwardvdesign.com>
2018-01-09 10:49:15 -0500

    Added jQuery ajax post method

--- main.js
+++ main.js
@@ -26,3 +26,17 @@ $.ajax({
     console.log(jqXHR);
   }
 });
+
+// jQuery POST request
+$.ajax({
+  url: 'https://api-to-call.com/endpoint',
+  type: 'POST',
+  data: JSON.stringify({id: 200}),
+  dataType: 'json',
+  success(response) {
+    console.log(response);
+  },
+  error(jqXHR, status, errorThrown) {
+    console.log(jqXHR);
+  }
+});
